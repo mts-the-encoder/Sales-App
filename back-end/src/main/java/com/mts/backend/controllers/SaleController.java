bad27eb78286013d4a64afd456a6262bb001c229
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-09-11 18:11:07 -0300

    Date select

--- back-end/src/main/java/com/mts/backend/controllers/SaleController.java
+++ back-end/src/main/java/com/mts/backend/controllers/SaleController.java
@@ -1,17 +1,16 @@
 package com.mts.backend.controllers;
 
 import com.mts.backend.entities.SaleEntity;
-import com.mts.backend.repositories.SaleRepository;
 import com.mts.backend.services.SaleService;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequestMapping("/sales")
@@ -23,7 +22,9 @@ public class SaleController {
     SaleService service;
 
     @GetMapping
-    public List<SaleEntity> findSales() {
-        return service.findSales();
+    public Page<SaleEntity> findSales(@RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                      @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+                                      Pageable pageable) {
+        return service.findSales(minDate, maxDate, pageable);
     }
 }
--- back-end/src/main/java/com/mts/backend/repositories/SaleRepository.java
+++ back-end/src/main/java/com/mts/backend/repositories/SaleRepository.java
@@ -1,7 +1,15 @@
 package com.mts.backend.repositories;
 
 import com.mts.backend.entities.SaleEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<SaleEntity, Long> {
+
+    @Query("SELECT obj FROM SaleEntity obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+    Page<SaleEntity> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
