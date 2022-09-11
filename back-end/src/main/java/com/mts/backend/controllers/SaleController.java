51ce25f180dc2c1b51be5c67269142438e6b1179
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-09-11 19:35:18 -0300

    twilio Sms

--- back-end/src/main/java/com/mts/backend/controllers/SaleController.java
+++ back-end/src/main/java/com/mts/backend/controllers/SaleController.java
@@ -2,29 +2,37 @@ package com.mts.backend.controllers;
 
 import com.mts.backend.entities.SaleEntity;
 import com.mts.backend.services.SaleService;
+import com.mts.backend.services.SmsService;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequestMapping("/sales")
 @Builder
 @AllArgsConstructor
+@Data
 public class SaleController {
 
     @Autowired
-    SaleService service;
+    private SaleService service;
+
+    @Autowired
+    private SmsService smsService;
 
     @GetMapping
     public Page<SaleEntity> findSales(@RequestParam(value = "minDate", defaultValue = "") String minDate,
                                       @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
                                       Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
