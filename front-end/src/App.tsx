2fcdef2b0f3b07963aa2791c6e22c467bb15f629
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-09-10 23:42:24 -0300

    add Sales Card

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,19 @@
-import NotificationButton from "./components/NotificationButton"
-import Header from "./components/Header"
+import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     //Fragment
     <>
       <Header/>
-      <NotificationButton/>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
